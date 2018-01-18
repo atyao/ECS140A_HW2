20c371e33854548fc0fab5fa362d57baeba5f715
John Nguyen <jolnguyen@ucdavis.edu>
2018-01-17 16:53:34 -0800

    Finish with part 11

--- Given/part11/t002.c
+++
@@ -1,8 +0,0 @@
-int esquare(int x){ return x*x;}
-#include <math.h>
-int esqrt(int x){ double y; if (x < 0) return 0; y = sqrt((double)x); return (int)y;}
-#include <stdio.h>
-int main() {
-{
-}
-return 0; }
