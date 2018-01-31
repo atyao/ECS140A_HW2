63791074d7f916323cebb411b6354c1861b203a7
ATYao <atyao@ucdavis.edu>
2018-01-30 16:47:06 -0800

    Commented 18 and 19

---
+++ HW2/part19/TK.java
@@ -0,0 +1,60 @@
+/* *** This file is given as part of the programming assignment. *** */
+
+// Token Kind (internal representations of tokens)
+
+public enum TK {
+
+    VAR,     // var
+    RAV,     // rav
+    PRINT,   // print
+    SKIP,    // skip
+    STOP,    // stop
+    IF,      // if
+    FI,      // fi
+    DO,      // do
+    OD,      // od
+    ELSE,    // else
+    FA,      // fa
+    AF,      // af
+    TO,      // to
+    ST,      // st
+  
+    ASSIGN,   // :=
+    LPAREN,   // (
+    RPAREN,   // )
+    PLUS,     // +
+    MINUS,    // -
+    TIMES,    // *
+    DIVIDE,   // /
+    MODULUS,  // %
+    MAX,      // Max
+    BREAK,    // Break
+    DUMP,     // Dump
+
+    SQUARE,   // ^
+    SQRT,     // @
+    COMMA,    // ,
+
+    EQ,       // =
+    NE,       // /=
+    LT,       // <
+    GT,       // >
+    LE,       // <=
+    GE,       // >=
+
+    ARROW,    // ->
+    BOX,      // []
+
+
+    ID,       // identifier
+    MODULO,   // MODULO
+    NUM,      // number
+
+    EOF,      // end of file
+
+    // ERROR special error token kind (for scanner to return to parser)
+    ERROR,
+    // none marks end of each first set in parsing.
+    // you might not need this.
+    none,
+}
